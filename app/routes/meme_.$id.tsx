0f9554bf47f33ab7daaa2cb75198c58b7e876805
Himanshu Bhardwaz <himanshu76200@gmail.com>
2023-05-07 16:53:35 +0530

    meme/ page updated

--- app/routes/meme_.$id.tsx
+++ app/routes/meme_.$id.tsx
@@ -1,16 +1,13 @@
-import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import type { LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import {
   getMemeById,
   getCaptionedImageUrl,
-  updateMeme,
 } from "~/utils/meme-services.server";
 import {
   isRouteErrorResponse,
   useRouteError,
   useLoaderData,
-  Form,
-  useNavigation,
 } from "@remix-run/react";
 
 export async function loader({ request, params }: LoaderArgs) {
@@ -30,37 +27,9 @@ export async function loader({ request, params }: LoaderArgs) {
 
 export default function MemeByIdPage() {
   const data = useLoaderData<typeof loader>() || {};
-  const navigation = useNavigation();
-
-  const isLoading = navigation.state === "submitting";
-
   return (
     <div className='flex min-h-[calc(100vh-8rem)] flex-col items-center justify-center p-8 gap-8'>
       <img src={data.captionedImageUrl} alt='' height='500' width='500' />
-      {/*<Form className='flex-shrink-0 w-full max-w-sm bg-base-100' method='POST'>
-        <div className='form-control'>
-          <label className='label' htmlFor='caption'>
-            <span className='label-text'>Edit caption</span>
-          </label>
-          <textarea
-            className='textarea textarea-bordered'
-            defaultValue={data.meme.caption.trim()}
-            name='caption'
-            data-gramm='false'
-            data-gramm_editor='false'
-            data-enable-grammarly='false'
-          />
-        </div>
-        <div className='form-control mt-6'>
-          <button
-            className='btn btn-primary w-full max-w-sm'
-            type='submit'
-            disabled={isLoading}
-          >
-            {isLoading ? "Loading..." : "Save caption"}
-          </button>
-        </div>
-      </Form>*/}
     </div>
   );
 }
